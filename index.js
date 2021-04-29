95244e45e5c1089943998a54cea4a797739096f8
miguelmar21 <miguelmar21@gmail.com>
2021-04-29 18:59:57 -0500

    initial

---
+++ index.js
@@ -0,0 +1,43 @@
+
+var searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+            var res = JSON.parse(xhttp.responseText);
+            var imageData = res.results.map(function(item){
+                return item;
+            })
+
+            var container = document.getElementById('image-divs');
+            container.innerHTML = '';
+
+            var resultsContainer = document.getElementById('show-results')
+            resultsContainer.innerHTML = '';
+            var showResults = document.createElement('div');
+            showResults.classList.add('show-results');
+            showResults.innerHTML = `
+                <h2>${`Showing results for: `} <span>${textValue.charAt(0).toUpperCase() + textValue.slice(1)}</span></h2>
+            `
+            resultsContainer.appendChild(showResults);
+
+            imageData.forEach(function(image){
+                console.log(image)
+                var imageDiv = document.createElement('div');
+                imageDiv.innerHTML = `
+                    <img src=${image.urls.regular}>
+                `
+                container.appendChild(imageDiv);
+            })
+        }
+    };
+
+var textValue = document.getElementById('search-bar').value;
+xhttp.open("GET", `https://api.unsplash.com/search/photos?page=1&query=${textValue}&client_id=58VkTwarYrsNBXEmBb2vMluNRjrK4agI7Rnk9jrBsRY`, true);
+xhttp.setRequestHeader('Authorization', '58VkTwarYrsNBXEmBb2vMluNRjrK4agI7Rnk9jrBsRY');
+xhttp.send();
+})
+
